e61cf39a9b8e6109dfaafd633560d7c1c4b688f0
Kankon Mondal <kankonnil@gmail.com>
2026-09-11 22:13:07 +0600

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
   allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.10.242"],
   images: {
     unoptimized: true,
